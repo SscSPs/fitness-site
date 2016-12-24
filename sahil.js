55d355e7b3759d1bf0774411d2fdf8fc0b190128
Sahil Soni <sscsps@gmail.com>
2016-12-24 19:55:32 +0530

    more

--- sahil.js
+++ sahil.js
@@ -1,11 +1,11 @@
 function loadHeader(headerId){
     var text = '<h1><a style="color: white; text-decoration: none;" href="/">Fitness Central</a></h1>';
     text+='<ul >';
-    text+='<li><a style="color: white; text-decoration: none;" href="/">Home</a></li>';
-    text+='<li><a style="color: white; text-decoration: none;" href="About.html">About</a></li>';
-    text+='<li><a style="color: white; text-decoration: none;" href="Contact.html">Contact Us</a></li>';
-    text+='<li><a style="color: white; text-decoration: none;" href="register.html">Register</a></li>';
-    text+='<li><a style="color: white; text-decoration: none;" href="Login.html">Login</a></li>';
+    text+='<a style="color: white; text-decoration: none;" href="/"><li>Home</li></a>';
+    text+='<a style="color: white; text-decoration: none;" href="About.html"><li>About</li></a>';
+    text+='<a style="color: white; text-decoration: none;" href="Contact.html"><li>Contact Us</li></a>';
+    text+='<a style="color: white; text-decoration: none;" href="register.html"><li>Register</li></a>';
+    text+='<a style="color: white; text-decoration: none;" href="Login.html"><li>Login</li></a>';
     text+='</ul>';
     document.getElementById(headerId).innerHTML = text;
 }
