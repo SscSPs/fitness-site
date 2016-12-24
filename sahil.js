ebed77c308253bc51caf97b4549bf0ac909e8899
Sahil Soni <sscsps@gmail.com>
2016-12-24 18:27:04 +0530

    re positioned the body to center and more tweeks
    removed the a style from index.css because it was causing other links to
    go white

--- sahil.js
+++ sahil.js
@@ -1,11 +1,11 @@
 function loadHeader(headerId){
-    var text = '<h1><a href="/">Fitness Central</a></h1>';
-    text+='<ul class="nav_bar">';
-    text+='<li><a href="/">Home</a></li>';
-    text+='<li><a href="About.html">About</a></li>';
-    text+='<li><a href="Contact.html">Contact Us</a></li>';
-    text+='<li><a href="register.html">Register</a></li>';
-    text+='<li><a href="Login.html">Login</a></li>';
+    var text = '<h1><a style="color: white; text-decoration: none;" href="/">Fitness Central</a></h1>';
+    text+='<ul >';
+    text+='<li><a style="color: white; text-decoration: none;" href="/">Home</a></li>';
+    text+='<li><a style="color: white; text-decoration: none;" href="About.html">About</a></li>';
+    text+='<li><a style="color: white; text-decoration: none;" href="Contact.html">Contact Us</a></li>';
+    text+='<li><a style="color: white; text-decoration: none;" href="register.html">Register</a></li>';
+    text+='<li><a style="color: white; text-decoration: none;" href="Login.html">Login</a></li>';
     text+='</ul>';
     document.getElementById(headerId).innerHTML = text;
 }
