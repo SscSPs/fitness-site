8095bfa9d26d090991288e22782131b177887a7b
Sahil Soni <sscsps@gmail.com>
2016-12-25 17:22:47 +0530

    more updates

--- exercise/chests.php
+++ exercise/chests.php
@@ -4,15 +4,15 @@
     <meta charset="utf-8">
     
     <head>
-        <script type="text/javascript" src="js/index.js"></script>
+        <script type="text/javascript" src="/js/index.js"></script>
         <script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
-        <script src="js/jquery.maphilight.min.js"></script>
-        <script src="js/sahil.js"></script>
+        <script src="/js/jquery.maphilight.min.js"></script>
+        <script src="/js/sahil.js"></script>
         
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-        <link href="css/index.css" rel="stylesheet">
+        <link href="/css/index.css" rel="stylesheet">
         
-        <title>Arms - Fitness Central</title>
+        <title>Chest - Fitness Central</title>
         
     </head>
     
