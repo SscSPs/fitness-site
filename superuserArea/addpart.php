8fd631016e595ba491303a79589a2b95d9fe05d7
Sahil Soni <sscsps@gmail.com>
2017-01-10 13:54:56 +0530

    fixed add parts

--- superuserArea/addpart.php
+++ superuserArea/addpart.php
@@ -5,22 +5,24 @@ if ($db->connect_error) {
 }
 if(isset($_POST['submitpart'])){
   $partName = mysqli_real_escape_string($db, $_POST['partName']);
-  $sqlq = $db->query("SELET * FROM project_body_parts WHERE name = $partname");
-  if($sqlq->num_rows==0)
-  {
+
+  $sql = "SELECT * FROM `project_body_parts` WHERE `project_body_parts`.`name` = '$partName'";
+  $result = $db->query($sql);
+
+  if ($result->num_rows > 0) {
+    echo $_POST['partName'] . " already exist";
+  }
+  else {
     $sql1 = $db->query("INSERT INTO project_body_parts(name, number_of_exercise) VALUE('$partName', 0)");
     if($sql1)
       {
         echo "Part Added Successfully";
       }
-    else{
-        echo "some error occured";
-    }
   }
 }
 
   echo '<form action="addpart.php" method="post">';
-  echo '<label for="partName" style="width:200px;display: inline-block;">Password</label>';
+  echo '<label for="partName" style="width:200px;display: inline-block;">Body Part Name</label>';
   echo '<input type="text" name="partName" id="partName" placeholder="Part Name" required/><br>';
   echo '<input type="submit" name="submitpart" value="Add Part" />';
   echo '</form>';
