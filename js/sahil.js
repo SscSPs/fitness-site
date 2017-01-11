05233d98fdef16cfefdcf6cc622551c220e66fc7
ananayssj <ananayssj@gmail.com>
2017-01-11 17:30:56 +0530

    Transformation

--- js/sahil.js
+++ js/sahil.js
@@ -2,11 +2,11 @@ function loadHeader(headerId) {
     var text = '<h1><a style="color: white; text-decoration: none;" href="/">Fitness Central</a></h1>';
     text += '<ul >';
     text += '<a style="color: white; text-decoration: none;" href="/"><li>Home</li></a>';
-    text += '<a style="color: white; text-decoration: none;" href="/About.html"><li>About</li></a>';
-    text += '<a style="color: white; text-decoration: none;" href="/Contact.html"><li>Contact Us</li></a>';
+    text += '<a style="color: white; text-decoration: none;" href="/About.php"><li>About</li></a>';
+    text += '<a style="color: white; text-decoration: none;" href="/Contact.php"><li>Contact Us</li></a>';
     text += '<a style="color: white; text-decoration: none;" href="/register.php"><li>Register</li></a>';
     text += '<a style="color: white; text-decoration: none;" href="/Login.php"><li>Login</li></a>';
-    text += '<a style="color: white; text-decoration: none;" href="/BMI.html"><li>BMI Calculator</li></a>';
+    text += '<a style="color: white; text-decoration: none;" href="/BMI.php"><li>BMI Calculator</li></a>';
     text += '<a style="color: white; text-decoration: none;" href="/workout_calendar.php"><li>Workout Calendar</li></a>';
     text += '</ul>';
     document.getElementById(headerId).innerHTML = text;
@@ -17,10 +17,10 @@ function loadHeaderRegistered(headerId) {
     text = '<h1><a style="color: white; text-decoration: none;" href="/">Fitness Central</a></h1>';
     text += '<ul >';
     text += '<a style="color: white; text-decoration: none;" href="/"><li>Home</li></a>';
-    text += '<a style="color: white; text-decoration: none;" href="/About.html"><li>About</li></a>';
-    text += '<a style="color: white; text-decoration: none;" href="/Contact.html"><li>Contact Us</li></a>';
+    text += '<a style="color: white; text-decoration: none;" href="/About.php"><li>About</li></a>';
+    text += '<a style="color: white; text-decoration: none;" href="/Contact.php"><li>Contact Us</li></a>';
     text += '<a style="color: white; text-decoration: none;" ><li>Logout</li></a>';
-    text += '<a style="color: white; text-decoration: none;" href="/BMI.html"><li>BMI Calculator</li></a>';
+    text += '<a style="color: white; text-decoration: none;" href="/BMI.php"><li>BMI Calculator</li></a>';
     text += '</ul>';
     document.getElementById(headerId).innerHTML = text;
 }
